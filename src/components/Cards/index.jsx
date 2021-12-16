ea4d0dd630a2e13ad694b57451d2b90c0fff9266
darkido <darkido256@gmail.com>
2021-12-16 08:47:41 +0330

    [chore]: change homepage to new card

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -5,9 +5,71 @@ import ousamaImage from "../../assets/images/ousama_ranking.webp";
 import { Menu, MenuItem, Typography, Button, Grid, Chip } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
-import {numberWithCommas} from '../../utils/helpers'
-export const MALCard = () => {
-  const animeData = {
+import { numberWithCommas } from "../../utils/helpers";
+
+export const MALCard = (animeData) => {
+  const anime = animeData.animeData;
+  console.log("anime Data", anime);
+  return (
+    <div className="mal-card">
+      <div className="mal-card__title">
+        <a
+          href={`https://myanimelist.net/anime/${anime.id}`}
+          className="link-title"
+        >
+          {anime.title}
+        </a>
+      </div>
+      <div className="mal-card__prodsrc">
+        <span className="producer">
+          <a
+            href={`https://myanimelist.net/anime/producer/${anime.studio.id}`}
+            title={anime.studio.name}
+          >
+            {anime.studio.name}
+          </a>
+        </span>
+        <div className="eps">
+          <span>
+          {anime.epi ? anime.epi : '?'} ep{anime.epi > 1 && "s"}
+          </span>
+        </div>
+        <span className="source">{anime.source}</span>
+      </div>
+      <div className="mal-card__genres">
+        {anime.genres.map((genre) => (
+          <Chip
+            label={genre.name}
+            size="small"
+            component={Link}
+            to={`/anime/genre/${genre.id}`}
+          />
+        ))}
+      </div>
+
+      <div className="mal-card__image">
+        <img src={anime.poster} width="167" alt={anime.title} />
+      </div>
+
+      <div className="mal-card__synopsis">{anime.synposis}</div>
+
+      <div className="mal-card__information">
+        <span className="info release-date">{anime.release_date}</span>
+        <span className="info">
+          <FontAwesomeIcon icon={["far", "star"]} className="mr4" />
+          {anime.score}
+        </span>
+        <span className="info">
+          <FontAwesomeIcon icon="user" className="mr4" />
+          {numberWithCommas(anime.members)}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export const AllCards = () => {
+  const anime = {
     id: 40834,
     title: "Ousama Ranking",
     studio: {
@@ -28,82 +90,17 @@ export const MALCard = () => {
     ],
     poster: ousamaImage,
     synposis: `        این داستان "جنگ های گل رز" که در قرون وسطا رخ داد را به تصویر می کشد
-    .نبردی خونین بین رزهای سفید یورک و رزهای سرخ لنستر بر سر پادشاهی
-    ...انگلستان. ریچارد پسر سوم خانواده یورک با رازی بزرگ متولد می شود.او...`,
-    release_date:"TV - Oct 15, 2021, 00:55 (JST) ",
-    score:8.86,
-    members:140820,
-
+        .نبردی خونین بین رزهای سفید یورک و رزهای سرخ لنستر بر سر پادشاهی
+        ...انگلستان. ریچارد پسر سوم خانواده یورک با رازی بزرگ متولد می شود.او...`,
+    release_date: "TV - Oct 15, 2021, 00:55 (JST) ",
+    score: 8.86,
+    members: 140820,
   };
-
-  return (
-    <div className="mal-card">
-      <div className="mal-card__title">
-        <a
-          href={`https://myanimelist.net/anime/${animeData.id}`}
-          className="link-title"
-        >
-          {animeData.title}
-        </a>
-      </div>
-      <div className="mal-card__prodsrc">
-        <span className="producer">
-          <a
-            href={`https://myanimelist.net/anime/producer/${animeData.studio.id}`}
-            title={animeData.studio.name}
-          >
-            {animeData.studio.name}
-          </a>
-        </span>
-        <div className="eps">
-          <span>
-            {animeData.epi} ep{animeData.epi > 1 && "s"}
-          </span>
-        </div>
-        <span className="source">{animeData.source}</span>
-      </div>
-      <div className="mal-card__genres">
-        {animeData.genres.map((genre) => (
-          <Chip
-            label={genre.name}
-            size="small"
-            component={Link}
-            to={`/anime/genre/${genre.id}`}
-          />
-        ))}
-      </div>
-
-      <div className="mal-card__image">
-        <img src={animeData.poster} width="167" alt={animeData.title} />
-      </div>
-
-      <div className="mal-card__synopsis">
-     {animeData.synposis}
-      </div>
-
-      <div className="mal-card__information">
-        <span className="info release-date">
-          {animeData.release_date}
-        </span>
-        <span className="info">
-          <FontAwesomeIcon icon={["far", "star"]} className="mr4" />
-          {animeData.score}
-        </span>
-        <span className="info">
-          <FontAwesomeIcon icon="user" className="mr4" />
-          {numberWithCommas(animeData.members)}
-        </span>
-      </div>
-    </div>
-  );
-};
-
-export const AllCards = () => {
   return (
     <div>
       <Grid
         container
-        style={{margin:'auto'}}
+        style={{ margin: "auto" }}
         alignItems="center"
         justifyContent="center"
         rowSpacing={4}
@@ -112,7 +109,7 @@ export const AllCards = () => {
       >
         {[...Array(6)].map((x, i) => (
           <Grid item md={6} sm={6} xs={12} xl={4} key={i}>
-            <MALCard />
+            <MALCard animeData={anime} />
           </Grid>
         ))}
       </Grid>
--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -2,27 +2,27 @@ import { Helmet } from "react-helmet";
 import "./style.scss";
 
 import * as React from "react";
-import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
-import IconButton from "@mui/material/IconButton";
-import SearchIcon from "@mui/icons-material/Search";
-import CloseIcon from "@mui/icons-material/Close";
 import { Box, ButtonGroup, Button, Grid, Typography } from "@mui/material";
 import { baseUrl } from "../../utils/constants";
 import axios from "../../utils/axiosConfig";
 import { useEffect, useState } from "react";
-import ReactLoading from 'react-loading';
+import ReactLoading from "react-loading";
+import ousamaImage from "../../assets/images/ousama_ranking.webp";
+import { MALCard } from "../Cards";
+import { amber } from "@mui/material/colors";
 
 const HomePage = () => {
   const [loading, setLoading] = useState(true);
-  const [animeData, setAnimeData] = useState([]);
+  const [animeRawData, setAnimeRawData] = useState([]);
   useEffect(() => {
     async function fetchData() {
       const res = await axios
-        .get(`${baseUrl}/anime/season/2021/fall?limit=3`)
+        .get(
+          `${baseUrl}/anime/season/2021/fall?limit=10&fields=id,title,studios,num_episodes,source,genres,broadcast,synopsis,mean,num_list_users`
+        )
         .then((response) => {
           console.log("animeData ", response.data);
-          setAnimeData(response.data.data);
+          setAnimeRawData(response.data.data);
           setLoading(false);
         })
         .catch((err) => {
@@ -32,6 +32,25 @@ const HomePage = () => {
     fetchData();
   }, []);
 
+  const createData = (input) => {
+    const anime = input.node;
+    console.log("anime", anime);
+    return {
+      id: anime.id,
+      title: anime.title,
+      studio: anime.studios[0],
+      epi: anime.num_episodes,
+      source: anime.source.replace("_"," "),
+      genres: anime.genres.slice(0,5),
+      poster: anime.main_picture.medium.replace("-cdn.myanimelist.net",".awdl.ml"),
+      synposis: anime.synopsis,
+      release_date: anime.broadcast ? `${anime.broadcast.day_of_the_week} ${anime.broadcast.start_time}`: "???",
+      score: anime.mean,
+      members: anime.num_list_users,
+    };
+  };
+
+  const animeList = animeRawData.map(createData);
   return (
     <div style={{ overflowX: "hidden" }}>
       <Helmet>
@@ -46,10 +65,10 @@ const HomePage = () => {
           انیمه های فصلی
         </Typography>
         {loading ? (
-          <div style={{ padding: "100px", display: "flex" }}>
+          <div style={{ padding: "100px", display: "flex",justifyContent:"center" }}>
             <ReactLoading
               type="spinningBubbles"
-              color="orangered"
+              color="blue"
               height={100}
               width={100}
             />
@@ -57,31 +76,17 @@ const HomePage = () => {
         ) : (
           <>
             <Grid
-              style={{ display: "flex" }}
-              justifyContent="center"
-              alignItems="center"
               container
+              style={{ margin: "auto" }}
+              alignItems="center"
+              justifyContent="center"
               rowSpacing={4}
-              md={8}
-              xs={10}
+              md={10}
+              xl={8.5}
             >
-              {animeData.map(anime => (
-                <Grid item md={4} sm={6} xs={12} >
-                  <Paper
-                    sx={{
-                      display: "flex",
-                      flexDirection: "row",
-                      width: "450px",
-                      height: "300px",
-                      marginTop: "62px",
-                      border: "5px solid black",
-                    }}
-                  >
-                    <img src={anime.node.main_picture.medium} />
-                    Name: {anime.node.title}
-                    <br />
-                    Year:
-                  </Paper>
+              {animeList.map((anime, i) => (
+                <Grid item md={6} sm={6} xs={12} xl={4} key={i}>
+                  <MALCard animeData={anime} />
                 </Grid>
               ))}
             </Grid>
